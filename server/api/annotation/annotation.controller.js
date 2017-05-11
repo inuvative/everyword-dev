5e1f2fcacf3808a64e8ea147dc191bf8a92b0813
dsimmons <derek.simmons@sensus.com>
2017-05-11 14:32:50 -0400

    Built everyword from commit 97d7937 on branch develop

--- server/api/annotation/annotation.controller.js
+++ server/api/annotation/annotation.controller.js
@@ -249,8 +249,8 @@ exports.getNewestAnnotation = function(req,res) {
 		  if(err){ return handleError(res,err);}
 		  var minCount = result && result[0] ? result[0].count : 0;
 		  var least=[];
-		  var i=1;
-		  while(i<result.length && result[i].count <= minCount){
+		  var i=0;
+		  while(i<10){//result.length && result[i].count <= minCount){
 			  least.push(mongooseTypes.ObjectId(result[i]._id));		
 			  i++;
 		  }
@@ -258,23 +258,27 @@ exports.getNewestAnnotation = function(req,res) {
 		  earliest.setDate(earliest.getDate()-7);
 		  
 		  FeedEntry.find({user : {$in: least},date:{$gte:earliest}})
-		  		   .populate('comment media reference')
+		  		   .populate('comment media reference user')
 		  		   .sort({"date":-1})
-		  		   .limit(1).exec(function(err,newest){
+		  		   .limit(1).exec(function(err,fe){
 		  			   if(err){
 		  				   return handleError(res,err);
 		  			   }
+		  			   if(!fe || fe.length===0) {
+		  				   return res.send(null);
+		  			   }
+		  			   var newest=fe[0];
 		  			   if(newest.comment){
-		  				   Comment.populate(e.comment, [{path: 'user', model: 'User'}],function(err,comment){
-		  					  return res.status(200).json(comment); 
+		  				   Comment.populate(newest.comment, [{path: 'user', model: 'User'}],function(err,comment){
+		  					  return res.status(200).json(newest); 
 		  				   });
 		  			   }else if(newest.media){
-		  				   Media.populate(e.media,[{path: 'user', model: 'User'},{path: 'image', model: 'Image'}],function(err,media){
-		  					   return res.status(200).json(media);
+		  				   Media.populate(newest.media,[{path: 'user', model: 'User'},{path: 'image', model: 'Image'}],function(err,media){
+		  					   return res.status(200).json(newest);
 		  				   });
 		  			   } else if(newest.reference){
-		  				   Reference.populate(e.media,[{path: 'user', model: 'User'}],function(err,reference){
-		  					   return res.status(200).json(reference);
+		  				   Reference.populate(newest.reference,[{path: 'user', model: 'User'}],function(err,reference){
+		  					   return res.status(200).json(newest);
 		  				   });
 		  			   } else {
 		  				   return res.send(null);
